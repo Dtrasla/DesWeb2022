aefeacd39662959dd50ab2ca6d0306aba17156dd
StebanVanegas <87554378+StebanVanegas@users.noreply.github.com>
2022-03-17 17:13:03 -0500

    Phone

--- src/main/java/com/webdev/productsystem/Tours/Booking/Domain/Booking.java
+++ src/main/java/com/webdev/productsystem/Tours/Booking/Domain/Booking.java
@@ -1,4 +1,30 @@
-package com.webdev.productsystem.Tours.Hotel.Domain;
+package com.webdev.productsystem.Tours.Booking.Domain;
 
-public class Hotel {
+import com.webdev.productsystem.Tours.Booking.Domain.ValueObjects;
+import com.webdev.productsystem.Tours.Hotel.Domain.ValueObjects;
+import com.webdev.productsystem.Tours.Tickets.Domain.ValueObjects;
+import com.webdev.productsystem.Tours.Tour.Domain.ValueObjects;
+
+public class Booking {
+    
+        public BookingId BookingId;
+    
+        public Booking(BookingId BookingId,BookingHotels BookingHotels, BookingTickets BookingTickets, BookingTour BookingTour) {
+            this.BookingId = BookingId;
+            this.BookingHotels = BookingHotels;
+            this.BookingTickets = BookingTickets;
+            this.BookingTour = BookingTour;
+            
+        }
+    
+        public static Booking create(BookingId BookingId,BookingHotels BookingHotels, BookingTickets BookingTickets, BookingTour BookingTour){
+            Booking Booking = new Booking(BookingId, BookingHotels, BookingTickets, BookingTour);
+            return Booking;
+        }
+    
+    
+    
+    
+        
+
 }
