7afcabb4e6d8da68ae9eebd562008d7a86ba2d7a
Daniel Traslavina <danstervan1@gmail.com>
2022-03-17 10:42:19 -0500

    Changes to User Class and Value Objects, Missing Phone and UserData Classes

--- src/main/java/com/webdev/productsystem/Users/User/Domain/User.java
+++ src/main/java/com/webdev/productsystem/Users/User/Domain/User.java
@@ -14,33 +14,25 @@ import java.util.stream.Collectors;
 public class User {
 
     private UserId userId;
-    private UserName userName;
     private UserEmail userEmail;
-    private UserIsSeller isSeller;
-    private UserBalance balance;
-    private UserRating rating;
-    private UserPassword password;
-    private Optional<List<UserAddress>> addressList;
 
-    public User(UserId userId, UserName userName, UserEmail userEmail, UserIsSeller isSeller, UserBalance balance, UserRating rating, UserPassword password,
-                Optional<List<UserAddress>> addressList) {
+    private UserPassword password;
+    private UserData userData;
+    private Phone userPhone;
+
+    public User(UserId userId,  UserEmail userEmail, UserPassword password, Phone userPhone, UserData userData) {
         this.userId = userId;
-        this.userName = userName;
         this.userEmail = userEmail;
-        this.isSeller = isSeller;
-        this.balance = balance;
-        this.rating = rating;
         this.password = password;
-        this.addressList = addressList;
+        this.userPhone = userPhone;
+        this.userData = userData;
     }
 
-    public static User create(UserId userId, UserName userName, UserEmail userEmail, UserPassword password) {
+    public static User create(UserId userId,  UserEmail userEmail, UserPassword password) {
         User user = new User(userId,
-                            userName,
                             userEmail,
-                            new UserIsSeller(false),
-                            new UserBalance(0d),
-                            new UserRating(5d),
+                            userPhone,
+                            userData,
                             password, Optional.empty());
         return user;
     }
@@ -54,25 +46,11 @@ public class User {
     public HashMap<String, Object> data() {
         HashMap<String, Object> data = new HashMap<>() {{
             put("id", userId.value());
-            put("name", userName.value());
             put("email", userEmail.value());
-            put("isSeller", isSeller.value());
-            put("balance", balance.value());
-            put("rating", rating.value() * 20);
-            put("address", createAddress());
+//          put("");
         }};
         return data;
     }
 
-    private List<HashMap<String, Object>> createAddress() {
-        List<HashMap<String, Object>> list = new ArrayList<>();
-        if (!addressList.isEmpty()) {
-            list = addressList.get().stream().map(address -> address.data()).collect(Collectors.toList());
-        }
-        return list;
-    }
 
-    private User() {
-        addressList = Optional.empty();
-    }
 }
