134f871bd4e73531a6d5a09fcc988c7a4dd6c61e
David Ramirez <ramirezmonroyd@gmail.com>
2022-04-23 18:38:58 -0500

    Corrected user entity

--- src/main/java/com/webdev/productsystem/Users/User/Domain/User.java
+++ src/main/java/com/webdev/productsystem/Users/User/Domain/User.java
@@ -1,14 +1,8 @@
 package com.webdev.productsystem.Users.User.Domain;
 
-<<<<<<< HEAD
-import com.webdev.productsystem.Users.User.Domain.Entities.*;
-=======
 import com.webdev.productsystem.Users.User.Domain.Entities.UserData;
 import com.webdev.productsystem.Users.User.Domain.Entities.UserPhone;
->>>>>>> main
-import com.webdev.productsystem.Users.User.Domain.Exceptions.AuthenticateFailed;
 import com.webdev.productsystem.Users.User.Domain.ValueObjects.*;
-import com.webdev.productsystem.Users.User.Phone.Domain.Phone;
 
 import java.util.HashMap;
 
@@ -16,7 +10,6 @@ public class User {
     private UserId id;
     private UserEmail email;
     private UserPassword password;
-<<<<<<< HEAD
     private UserName name;
     private UserLastName lastName;
     private UserBirthday birthday;
@@ -27,14 +20,6 @@ public class User {
                 UserLastName lastName, UserBirthday birthday, UserGender gender, UserPhone phone) {
         this.id = id;
         this.email = email;
-=======
-    private UserData userData;
-    private UserPhone userPhone;
-
-    public User(UserId userId, UserEmail userEmail, UserPassword password, UserPhone userPhone, UserData userData) {
-        this.userId = userId;
-        this.userEmail = userEmail;
->>>>>>> main
         this.password = password;
         this.name = name;
         this.lastName = lastName;
@@ -43,20 +28,7 @@ public class User {
         this.phone = phone;
     }
 
-<<<<<<< HEAD
     private HashMap<String, Object> createPhone() { return phone.data(); }
-=======
-    public User(UserId userId, UserEmail userEmail, UserPassword password) {
-        this.userId = userId;
-        this.userEmail = userEmail;
-        this.password = password;
-    }
-
-    public static User create(UserId userId, UserEmail userEmail, UserPassword password) {
-        User user = new User(userId, userEmail, password);
-        return user;
-    }
->>>>>>> main
 
     public User create(UserId id, UserEmail email, UserPassword password, UserName name,
                        UserLastName lastName, UserBirthday birthday, UserGender gender, UserPhone phone) {
