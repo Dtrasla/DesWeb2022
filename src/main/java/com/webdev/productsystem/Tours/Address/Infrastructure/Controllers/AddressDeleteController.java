e277e19dd8531996a75bcdf3819b9dffcd6bb47d
David Ramirez <ramirezmonroyd@gmail.com>
2022-05-19 16:01:42 -0500

    Address delete controller documeted

--- src/main/java/com/webdev/productsystem/Tours/Address/Infrastructure/Controllers/AddressDeleteController.java
+++ src/main/java/com/webdev/productsystem/Tours/Address/Infrastructure/Controllers/AddressDeleteController.java
@@ -1,7 +1,14 @@
 package com.webdev.productsystem.Tours.Address.Infrastructure.Controllers;
 
+import com.webdev.productsystem.Shared.Infrastruture.Schema.ErrorSchema;
 import com.webdev.productsystem.Tours.Address.Application.Delete.AddressRemover;
 import com.webdev.productsystem.Tours.Address.Domain.Excpetions.AddressNotExists;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,10 +18,16 @@ import java.util.HashMap;
 
 @RestController
 @RequestMapping(value = "/address")
+@Tag(name = "Address", description = "Address REST API")
 public class AddressDeleteController {
     @Autowired
     private AddressRemover remover;
 
+    @Operation(summary = "Delete an address", description = "Delete an address that already exits in the system", tags = {"Address"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Address deleted"),
+            @ApiResponse(responseCode = "404", description = "Address not found", content = @Content(schema = @Schema(implementation = ErrorSchema.class))),
+    })
     @DeleteMapping(value = "/delete/{addressId}")
     public ResponseEntity execute(@PathVariable(value = "addressId") String id) {
         remover.execute(id);
