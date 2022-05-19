e1ad63c15355bb4d439ce438f2b1af821581d52e
David Ramirez <ramirezmonroyd@gmail.com>
2022-05-19 16:08:10 -0500

    Address find by id controller updated

--- src/main/java/com/webdev/productsystem/Tours/Address/Infrastructure/Controllers/AddressFindByIdController.java
+++ src/main/java/com/webdev/productsystem/Tours/Address/Infrastructure/Controllers/AddressFindByIdController.java
@@ -1,7 +1,14 @@
 package com.webdev.productsystem.Tours.Address.Infrastructure.Controllers;
 
+import com.webdev.productsystem.Shared.Infrastruture.Schema.ErrorSchema;
 import com.webdev.productsystem.Tours.Address.Application.Find.AddressByIdFinder;
 import com.webdev.productsystem.Tours.Address.Domain.Excpetions.AddressNotExists;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,10 +18,16 @@ import java.util.HashMap;
 
 @RestController
 @RequestMapping(value = "/address")
+@Tag(name = "Address", description = "Address REST API")
 public class AddressFindByIdController {
     @Autowired
     private AddressByIdFinder finder;
 
+    @Operation(summary = "Find a address by id", description = "Find and retrieves a address if it's id is found in the system", tags = {"Address"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Address found"),
+            @ApiResponse(responseCode = "404", description = "Address not found", content = @Content(schema = @Schema(implementation = ErrorSchema.class))),
+    })
     @GetMapping("/{addressId}")
     public ResponseEntity<HashMap<String, Object>> execute(@PathVariable(value = "addressId") String id) {
         return ResponseEntity.status(HttpStatus.OK).body(finder.execute(id).data());
