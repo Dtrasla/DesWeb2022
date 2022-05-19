44b3b02cce3d5df58460754067f89e530acd19a9
David Ramirez <ramirezmonroyd@gmail.com>
2022-05-19 15:41:27 -0500

    Hotel create controller documented

--- src/main/java/com/webdev/productsystem/Tours/Hotel/Infrastructure/Controllers/HotelCreateController.java
+++ src/main/java/com/webdev/productsystem/Tours/Hotel/Infrastructure/Controllers/HotelCreateController.java
@@ -1,9 +1,16 @@
 package com.webdev.productsystem.Tours.Hotel.Infrastructure.Controllers;
 
+import com.webdev.productsystem.Shared.Infrastruture.Schema.ErrorSchema;
 import com.webdev.productsystem.Tours.Hotel.Application.Create.HotelCreator;
 import com.webdev.productsystem.Tours.Hotel.Domain.Exceptions.HotelAlreadyExists;
 import com.webdev.productsystem.Tours.Hotel.Domain.Exceptions.HotelNameInvalidLength;
 import com.webdev.productsystem.Tours.Hotel.Domain.Exceptions.HotelNotExists;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +20,17 @@ import java.util.HashMap;
 
 @RestController
 @RequestMapping(value = "/hotel")
+@Tag(name = "hotel", description = "Hotel REST API")
 public class HotelCreateController {
     @Autowired
     private HotelCreator creator;
 
+    @Operation(summary = "Create new hotel", description = "Add a new hotel to the system", tags = {"Hotel"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Hotel created"),
+            @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content(schema = @Schema(implementation = ErrorSchema.class))),
+            @ApiResponse(responseCode = "409", description = "Hotel already exists", content = @Content(schema = @Schema(implementation = ErrorSchema.class)))
+    })
     @PostMapping(value = "/create")
     public ResponseEntity execute(@RequestBody HotelCreatorRequest request) {
         creator.execute(request.getId(), request.getName());
@@ -40,7 +54,10 @@ public class HotelCreateController {
     }
 
     static class HotelCreatorRequest {
+        @Schema(description = "Hotel Id", example = "e255f441-70a6-4e6a-896a-dac35607a4bd")
         private String id;
+
+        @Schema(description = "Hotel name, between 5 and 25 characters", example = "Marriott Bogotá")
         private String name;
 
         public String getId() {
