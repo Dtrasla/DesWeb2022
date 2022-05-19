5b0cf95d42493089fffd8e55cedaabff4ccd620b
David Ramirez <ramirezmonroyd@gmail.com>
2022-05-19 16:05:15 -0500

    Address find all controller documented

--- src/main/java/com/webdev/productsystem/Tours/Address/Infrastructure/Controllers/AddressFindAllController.java
+++ src/main/java/com/webdev/productsystem/Tours/Address/Infrastructure/Controllers/AddressFindAllController.java
@@ -2,6 +2,10 @@ package com.webdev.productsystem.Tours.Address.Infrastructure.Controllers;
 
 import com.webdev.productsystem.Tours.Address.Application.All.AddressFindAll;
 import com.webdev.productsystem.Tours.Address.Application.All.AddressFindAllResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +18,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/address")
+@Tag(name = "Address", description = "Address REST API")
 public class AddressFindAllController {
     @Autowired
     private AddressFindAll all;
 
+    @Operation(summary = "Get all the addresses", description = "Find all the addresses stored in the system", tags = {"Address"})
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Addresses found") })
     @GetMapping(value = "/all")
     public ResponseEntity<List<HashMap<String, Object>>> execute() {
         return ResponseEntity.status(HttpStatus.OK).body(new AddressFindAllResponse(all.execute()).response());
