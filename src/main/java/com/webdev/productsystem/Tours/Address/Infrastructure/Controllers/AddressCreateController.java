2951d24d92c3e48543a86c09c2bbb3decb26f804
David Ramirez <ramirezmonroyd@gmail.com>
2022-05-19 15:57:40 -0500

    Address create controller documented

--- src/main/java/com/webdev/productsystem/Tours/Address/Infrastructure/Controllers/AddressCreateController.java
+++ src/main/java/com/webdev/productsystem/Tours/Address/Infrastructure/Controllers/AddressCreateController.java
@@ -1,7 +1,14 @@
 package com.webdev.productsystem.Tours.Address.Infrastructure.Controllers;
 
+import com.webdev.productsystem.Shared.Infrastruture.Schema.ErrorSchema;
 import com.webdev.productsystem.Tours.Address.Application.Create.AddressCreator;
 import com.webdev.productsystem.Tours.Hotel.Domain.Exceptions.HotelAlreadyExists;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,10 +18,16 @@ import java.util.HashMap;
 
 @RestController
 @RequestMapping(value = "/address")
+@Tag(name = "Address", description = "Address REST API")
 public class AddressCreateController {
     @Autowired
     private AddressCreator creator;
 
+    @Operation(summary = "Create a new address", description = "Add a new address to the system", tags = {"Hotel"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Address created"),
+            @ApiResponse(responseCode = "409", description = "Address already exists", content = @Content(schema = @Schema(implementation = ErrorSchema.class)))
+    })
     @PostMapping(value = "/create")
     public ResponseEntity execute(@RequestBody AddressCreatorRequest request) {
         creator.execute(request.id, request.data, request.zipCode, request.cityId);
@@ -30,9 +43,16 @@ public class AddressCreateController {
     }
 
     static class AddressCreatorRequest {
+        @Schema(description = "Address id", example = "e255f441-70a6-4e6a-896a-dac35607a4bd")
         String id;
+
+        @Schema(description = "Address data", example = "Cl. 152 # 58C - 50")
         String data;
+
+        @Schema(description = "Address zip code", example = "111156")
         String zipCode;
+
+        @Schema(description = "Address city id", example = "e255f441-70a6-4e6a-896a-dac35607a4bd")
         String cityId;
 
         public String getId() {
