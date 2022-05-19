7b0e0576b9d59da0f1e8fc0fc0cbd992ba4e7385
David Ramirez <ramirezmonroyd@gmail.com>
2022-05-19 15:46:55 -0500

    Hotel find by id controller documented

--- src/main/java/com/webdev/productsystem/Tours/Hotel/Infrastructure/Controllers/HotelFindByIdController.java
+++ src/main/java/com/webdev/productsystem/Tours/Hotel/Infrastructure/Controllers/HotelFindByIdController.java
@@ -1,8 +1,15 @@
 
 package com.webdev.productsystem.Tours.Hotel.Infrastructure.Controllers;
 
+import com.webdev.productsystem.Shared.Infrastruture.Schema.ErrorSchema;
 import com.webdev.productsystem.Tours.Hotel.Application.Find.HotelByIdFinder;
 import com.webdev.productsystem.Tours.Hotel.Domain.Exceptions.HotelNotExists;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +19,16 @@ import java.util.HashMap;
 
 @RestController
 @RequestMapping(value = "/hotel")
+@Tag(name = "hotel", description = "Hotel REST API")
 public class HotelFindByIdController {
     @Autowired
     private HotelByIdFinder finder;
 
+    @Operation(summary = "Delete a hotel", description = "Delete a hotel that already exits in the system", tags = {"Hotel"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Hotel found"),
+            @ApiResponse(responseCode = "404", description = "Hotel doesn't exist", content = @Content(schema = @Schema(implementation = ErrorSchema.class))),
+    })
     @GetMapping("/{hotelId}")
     public ResponseEntity<HashMap<String, Object>> execute(@PathVariable(value = "hotelId") String id) {
         return ResponseEntity.status(HttpStatus.OK).body(finder.execute(id).data());
