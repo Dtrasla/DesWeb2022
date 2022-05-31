0a5e01d09206e0cc174e4c37fe5a6221a6ceaf03
Daniel Traslavina <danstervan1@gmail.com>
2022-05-31 18:33:53 -0500

    Added Execute command for AddPhone

--- src/main/java/com/webdev/productsystem/Users/User/Application/AddPhone/AddPhone.java
+++ src/main/java/com/webdev/productsystem/Users/User/Application/AddPhone/AddPhone.java
@@ -2,6 +2,12 @@ package com.webdev.productsystem.Users.User.Application.AddPhone;
 
 
 import com.webdev.productsystem.Users.Phone.Domain.Ports.PhoneRepository;
+import com.webdev.productsystem.Users.User.Domain.Entities.UserPhone;
+import com.webdev.productsystem.Users.User.Domain.Ports.UserRepository;
+import com.webdev.productsystem.Users.User.Domain.User;
+import com.webdev.productsystem.Users.User.Domain.ValueObjects.UserId;
+
+import java.util.Optional;
 
 
 /* TODO
@@ -9,13 +15,21 @@ Acabar esta clase
  */
 
 public class AddPhone {
-    private PhoneRepository repository;
+    private UserRepository repository;
 
-    public AddPhone(PhoneRepository repository) {
+    public AddPhone(UserRepository repository) {
         this.repository = repository;
     }
 
-    public void execute() {
+    public void execute(String userId, String phoneCountryCode, String phoneNumber, String phoneId) {
+        Optional<User> optionalUser = repository.find(new UserId(userId));
+        if(optionalUser.isPresent()){
+            User user = optionalUser.get();
+            user.addPhone(new UserPhone(phoneId, phoneCountryCode, phoneNumber));
+            this.repository.update(user);
+
+        }
+
 
 
     }
