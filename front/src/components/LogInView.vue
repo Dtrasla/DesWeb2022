2dced29f14da664e12e3cc1640eca2bc672a326f
Santiago Diaz <diazsantiago1605@gmail.com>
2022-06-02 13:59:15 -0500

    Fix password login

--- front/src/components/LogInView.vue
+++ front/src/components/LogInView.vue
@@ -1,30 +1,34 @@
 <template>
-  <v-form
+ <v-form
     ref="form"
     v-model="valid"
     lazy-validation
   >
     <v-text-field
-      v-model="name"
-      :rules="nameRules"
-      label="Correo"
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
       required
     ></v-text-field>
 
-     <v-text-field
-      v-model="password"
-       type="password"
-      :rules="nameRules"
-      label="Contraseña"
-      required
-    ></v-text-field>
+    <v-text-field
+            v-model="password"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.min]"
+            :type="show1 ? 'text' : 'password'"
+            name="input-10-1"
+            label="Contraseña"
+
+            counter
+            @click:append="show1 = !show1"
+          ></v-text-field>
 
 
     <v-btn
       :disabled="!valid"
       color="success"
       class="mr-4"
-      @click="Ingresar"
+      @click="validate"
     >
       Ingresar
     </v-btn>
@@ -32,9 +36,41 @@
 
     <v-btn
       color="warning"
-      @click="resetValidation"
+      @click="registrar"
     >
       Registrar nuevo usuario
     </v-btn>
   </v-form>
+
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        show1: false,
+        password: 'Password',
+        rules: {
+          required: value => !!value || 'Required.',
+          emailMatch: () => (`The email and password you entered don't match`),
+        },
+      }
+    },
+
+    data1: () => ({
+      valid: true,
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ]
+      
+    }),
+    methods: {
+      validate () {
+        this.$refs.form.validate()
+      },
+    }
+
+  }
+  
+</script>
