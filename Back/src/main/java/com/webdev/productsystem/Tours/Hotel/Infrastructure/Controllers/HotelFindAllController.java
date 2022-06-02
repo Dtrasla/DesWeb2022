640ee53e3c549252cdcd47f0ce0fdb5e67dd285d
David Ramirez <ramirezmonroyd@gmail.com>
2022-06-02 01:10:34 -0500

    Added CORS control

--- Back/src/main/java/com/webdev/productsystem/Tours/Hotel/Infrastructure/Controllers/HotelFindAllController.java
+++ Back/src/main/java/com/webdev/productsystem/Tours/Hotel/Infrastructure/Controllers/HotelFindAllController.java
@@ -12,15 +12,14 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/hotel")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET})
 @Tag(name = "Hotel", description = "Hotel REST API")
 public class HotelFindAllController {
     @Autowired
